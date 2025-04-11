4dc0eda5ba4cb6e5001f5896d531d4f038e77fbd
SamTusick <stusick@outlook.com>
2025-04-11 15:50:55 -0400

    Binary Search

---
+++ Searching/Binary Search/binarySearch.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+int binarySearch(std::vector<int> array, int target);
+int main()
+{
+    std::vector<int> array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int target = 3;
+    int result = binarySearch(array, target);
+
+    if(result != -1)
+    {
+        std::cout << result << "\n";
+    }
+    else
+    {
+        std::cout << "No such element\n";
+    }
+    return 0;
+}
+
+int binarySearch(std::vector<int> array, int target)
+{
+    int low = 0;
+    int high = array.size() - 1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (array[mid] == target)
+        {
+            return mid;
+        }
+
+        if(array[mid] > target)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
