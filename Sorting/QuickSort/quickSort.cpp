8e3db30358d5a9f460d219a92cd31ad3240a21d6
SamTusick <stusick@outlook.com>
2025-04-11 19:46:48 -0400

    Adding a Remove Duplicate Function

--- Sorting/QuickSort/quickSort.cpp
+++ Sorting/QuickSort/quickSort.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <list>
 #include <vector>
 
 void swap(int &a , int &b);
 int partition(std::vector<int>& list_vector, int low, int high);
 void quickSort(std::vector<int>& list_vector, int low, int high);
-void printVector(const std::vector<int>& vec); //used for testing
+void printVector(const std::vector<int>& list_vector); //used for testing
+void removeDuplicates(std::vector<int>& list_vector);
 
 int main()
 {
-    std:: vector<int> myList = {2,8,9,10,12,3,5,7,6,4,1};
+    std:: vector<int> myList = {2,8,9,10,12,3,5,7,6,4,1,6};
 
     std::cout << "Original List: "; //used for testing
     printVector(myList); //used for testing
@@ -41,7 +43,7 @@ int partition(std::vector<int>& list_vector, int low, int high)
     for(int j = low; j < high; j++)
     {
         //when j < pivot -> swap and i++
-       if(list_vector[j] <= pivot)
+       if(list_vector[j] < pivot)
         {
            //then swap i and j
            swap(list_vector[i],list_vector[j]);
@@ -62,13 +64,27 @@ void quickSort(std::vector<int>& list_vector, int low, int high)
         int pivotIndex = partition(list_vector, low, high);
         quickSort(list_vector, low, pivotIndex - 1);
         quickSort(list_vector, pivotIndex + 1, high);
+        removeDuplicates(list_vector);
     }
 }
 
-void printVector(const std::vector<int>& vec) { //used for testing
-    for (size_t i = 0; i < vec.size(); ++i) {
-        std::cout << vec[i] << " ";
+void printVector(const std::vector<int>& list_vector) { //used for testing
+    for (size_t i = 0; i < list_vector.size(); ++i) {
+        std::cout << list_vector[i] << " ";
     }
     std::cout << std::endl;
+}
+
+void removeDuplicates(std::vector<int>& list_vector)
+{
+    for (int i = 0; i < list_vector.size() - 1 ; i++ )
+    {
+        if (list_vector[i] == list_vector[i+1])
+        {
+            list_vector.erase(list_vector.begin() + i);
+            --i;
+        }
+    }
+
 }
 
