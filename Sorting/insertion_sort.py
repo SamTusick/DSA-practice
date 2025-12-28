0879ebe3946484b0117e719664cb16b0f607e85c
SamTusick <stusick@outlook.com>
2025-12-27 21:58:31 -0500

    Set up file

--- Sorting/insertion_sort.py
+++ Sorting/insertion_sort.py
@@ -1,1 +1,13 @@
 # Insertion Sort
+
+def insertion_sort(arr):
+    return arr
+
+
+def main():
+    test_arr = [112, 64, 34, 25, 12, 22, 11, 32,  90]
+    sorted_arr = insertion_sort(test_arr)
+    print(sorted_arr)
+
+if __name__ == "__main__":
+    main()
