da145f127a169e24265011daea8a04e8da75bf6e
SamTusick <stusick@outlook.com>
2025-12-27 22:29:20 -0500

    Set up Merge Sort file

---
+++ Sorting/merge_sort.py
@@ -0,0 +1,10 @@
+def merge_sort(arr):
+    return arr
+
+def main():
+    sample_array = [112, 64, 34, 25, 12, 22, 11, 32,  90]
+    sorted_array = merge_sort(sample_array)
+    print("Sorted array is:", sorted_array)
+
+if __name__ == "__main__":
+    main()
