383efcb20f9a5109498718612bea9364baf5557c
SamTusick <stusick@outlook.com>
2025-12-27 22:19:30 -0500

    Complete insertion sort

--- Sorting/insertion_sort.py
+++ Sorting/insertion_sort.py
@@ -1,11 +1,16 @@
 # Insertion Sort
-
 def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i 
+        while (arr[j] < arr[j - 1] and j > 0):
+            temp = arr[j]
+            arr[j] = arr[j-1]
+            arr[j-1] = temp
+            j -= 1
     return arr
 
-
 def main():
-    test_arr = [112, 64, 34, 25, 12, 22, 11, 32,  90]
+    test_arr = [112, 64, 34, 25, 12, 22, 11, 32, 90]
     sorted_arr = insertion_sort(test_arr)
     print(sorted_arr)
 
