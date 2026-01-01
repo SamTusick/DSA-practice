891e5de74ce9fdd487df90ee2b8f81a82d4f194d
SamTusick <stusick@outlook.com>
2025-12-31 19:36:26 -0500

    Initialize Quick Sort

---
+++ Sorting/quick_sort.py
@@ -0,0 +1,15 @@
+# Quick Sort
+
+def quick_sort():
+    return
+
+
+def main():
+    sample_array = [112, 64, 34, 25, 12, 22, 11, 32,  90]
+    print("Starting Array: ", sample_array)
+    sorted_array = quick_sort(sample_array)
+    print("Sorted array is:", sorted_array)
+    return
+
+if __name__ == "__main__":
+    main()
