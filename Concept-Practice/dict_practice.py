246d598fb7ca0753e575917c0615daf427fdede9
SamTusick <stusick@outlook.com>
2026-01-10 18:20:13 -0500

    Working on dict practice

--- Concept-Practice/dict_practice.py
+++ Concept-Practice/dict_practice.py
@@ -1,11 +1,29 @@
 
 def main():
-    people = {}
-    people["name"] = "Sam"
-    people["age"] = 20
+    people = []
+    people.append({'name': 'Sam', 'age': 20, 'major': 'SWE'})
+    people.append({'name': 'Erik', 'age': 19, 'major': 'Prenursing'})
 
-    print(people)
+    #print(people)
 
+    #for person in people:
+        #if person['name'] == 'Erik':
+            #print(person)
+
+
+    people_dict = {
+        'Sam': {'age': 20, 'major': 'SWE'}, 
+        'Erik': {'age': 19, 'major': 'Prenursing'}
+    }
+
+    if 'Will' in people_dict:
+        print("WooHoo")
+    else:
+        print('Nope')
+
+    print(people_dict)
+    print(people_dict['Sam'])
+    print(people_dict['Erik']['age'])
 
 if __name__ == "__main__":
     main()
