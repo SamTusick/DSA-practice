ddd286a52650bf098ba356effc3e0efe3fcd96a9
SamTusick <stusick@outlook.com>
2026-01-12 11:49:08 -0500

    Basic Linked List syntax and logic

--- Concept-Practice/linked_list.py
+++ Concept-Practice/linked_list.py
@@ -1,5 +1,21 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 def main():
-    print('Hello')
+    n1 = ListNode(4)
+    n2 = ListNode(7)
+    n3 = ListNode(9)
+    n1.next = n2
+    n2.next = n3
+
+
+    current = n1
+
+    while current is not None:
+        print(current.val)
+        current = current.next
 
 if __name__ == '__main__':
     main()
