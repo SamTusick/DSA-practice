596cd71c11e1972c9080cec399f3d8d28c62ff1e
SamTusick <stusick@outlook.com>
2026-01-12 22:15:31 -0500

    Basic Linked List functions

--- Concept-Practice/linked_list.py
+++ Concept-Practice/linked_list.py
@@ -4,18 +4,54 @@ class ListNode(object):
         self.next = next
 
 def main():
-    n1 = ListNode(4)
-    n2 = ListNode(7)
-    n3 = ListNode(9)
-    n1.next = n2
-    n2.next = n3
+    head = ListNode(1)
+
+    append(head, 5)
+    append(head, 6)
+    append(head, 7)
+    remove(head, 6)
+    append(head, 6)
+
+    display(head)
+
+def append(head, value):
+
+    if head is None:
+        return ListNode(value)
+    
+    curr = head
+
+    while curr.next is not None:
+        curr = curr.next
+    curr.next = ListNode(value)
+
+def remove(head, value):
+    while head.val is not None and head.val == value:
+        head = head.next
+
+    curr = head
+
+    while curr.next is not None:
+        if curr.next.val == value:
+            curr.next = curr.next.next
+        curr = curr.next
 
 
-    current = n1
+def display(head):
+    values = []
 
-    while current is not None:
-        print(current.val)
-        current = current.next
+    if head is None:
+        return values
+    
+    curr = head
+
+    while curr is not None:
+        values.append(curr.val)
+        curr = curr.next
+    
+    print(values)
+
+
 
 if __name__ == '__main__':
     main()
