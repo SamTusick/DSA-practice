a70ec7c15885761dad22a9cfdaed0a14064319e0
SamTusick <stusick@outlook.com>
2026-01-18 12:48:54 -0500

    Define traversal methods

---
+++ Concept-Practice/binary_tree.py
@@ -0,0 +1,15 @@
+# Types: 
+#   Binary Tree - Each node has <= 2 childern
+#   Binary Search Tree (BST) - left < node < right
+#   Balanced Tree - Height is controlled
+#   Complete Tree - Filled left to right 
+#   Full Tree - Each node has 0 or 2 childern 
+
+# Traversal:
+#   DFS(Depth First):
+#       Preorder(Root,left,right)
+#       Inorder(left,root,right)
+#       Postorder(left,right,root)
+#   BFS(Level Order):
+#       Use a queue
+#       
