1486d090851f12081da6cf46a7b5326d8df1a38e
SamTusick <stusick@outlook.com>
2026-01-18 12:57:04 -0500

    Set up node class

--- Concept-Practice/trees.py
+++ Concept-Practice/trees.py
@@ -12,4 +12,15 @@
 #       Postorder(left,right,root)
 #   BFS(Level Order):
 #       Use a queue
-#       
+
+class Node(object):
+    def __init__(self,val=0,left=None,right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def main():
+    return 
+
+if __name__ == "__main__":
+    main()
